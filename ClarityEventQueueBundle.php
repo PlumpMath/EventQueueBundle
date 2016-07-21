8a4255c897211dcf3cf46600c7b1692202732245
varloc2000 <varloc2000@gmail.com>
2016-07-21 15:10:24 +0300

    Initial commit.

---
+++ ClarityEventQueueBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Clarity\EventQueueBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author varloc2000 <varloc2000@gmail.com>
+ */
+class ClarityEventQueueBundle extends Bundle
+{
+}
